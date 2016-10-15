1e6d059e42a5b9d7233ba49e12cd8dbf1132fa30
hhazeley <hhazeley@hazelnest.com>
2016-10-15 09:47:15 -0700

    Stop - Start V2 VM
    This scripts can be used to automate stopping and starting of Azure
    virtual machine. Import this into Azure Automation as Runbooks

---
+++ Stop.ps1
@@ -0,0 +1,85 @@
+
+Param(
+   $AutomationRG = "My1StRG",
+   $AutomationAcct = "StopStart",
+   $StopType = "Auto"
+)
+
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+function Get-AzureRmVMStatus {
+  [CmdletBinding()]
+  param (
+    #The name of a resouce group in your subscription
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceGroupName
+    ,
+    #VM name filter
+    [Parameter()]
+    [string]
+    $Name = '*'
+  )
+  Get-AzureRmVM -ResourceGroupName $ResourceGroupName | ? {$_.Tags.Keys -eq "Stop" -and $_.Tags.Values -eq "$StopType"} |
+    Get-AzureRmVM -Status |
+    Select-Object -Property Name, Statuses, ResourceGroupName |
+    Where-Object {$_.Name -like $Name} |
+    ForEach-Object {
+      $VMName = $_.Name
+      $ResourceGroupName = $_.ResourceGroupName
+      $_.Statuses |
+        Where-Object {$_.Code -like 'PowerState/*'} |
+        ForEach-Object {
+          New-Object -TypeName psobject -Property @{
+            Name   = $VMName
+            ResourceGroupName = $ResourceGroupName
+            Status = $_.DisplayStatus
+          }
+        }
+      }
+}
+
+$subs = Get-AzureRmSubscription
+
+$subs | % {Select-AzureRmSubscription -SubscriptionId $_.SubscriptionId
+$VMs = Get-AzureRmResourceGroup | % {Get-AzureRmVMStatus $_.ResourceGroupName}
+
+$ASVMs = $VMs | ? {$_.Status -ne "VM deallocated"}
+
+Foreach ($ASVM in $ASVMs)
+ {
+     $params = @{"Name"=$ASVM.name;"ResourceGroupName"=$ASVM.ResourceGroupName}
+     $msg = "Stopping VM "
+     $msg += $ASVM.name
+     $msg += " on resouregroup "
+     $msg += $ASVM.ResourceGroupName
+     $msg += "......"
+     Write-Output " " 
+     Write-Output $msg
+     Write-Output " "
+     Start-AzureRmAutomationRunbook	-AutomationAccountName $AutomationAcct -Name "StopVM" -Parameters $params -ResourceGroupName $AutomationRG
+     }
+}
---
+++ StopVM.ps1
@@ -0,0 +1,34 @@
+
+Param(
+   $Name,
+   $ResourceGroupName
+)
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+Write-Output " "
+Write-Output "Stopping VM $name on ResourceGroup $ResourceGroupname......"
+Write-Output " "
+Stop-AzureRmVM -Name $Name -ResourceGroupName $ResourceGroupName -Force
